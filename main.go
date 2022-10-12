3bb9b63d46461f3270b6c33cecd02561d8260e35
Dudude-bit <56934839+Dudude-bit@users.noreply.github.com>
2022-10-12 14:13:10 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
+package main
 
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var counter = map[string]int{}
+	var keys []string
+	var input_string string
+	_, err := fmt.Scanln(&input_string)
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < len(input_string); i++ {
+		if _, ok := counter[string(input_string[i])]; !ok {
+			keys = append(keys, string(input_string[i]))
+		}
+		counter[string(input_string[i])]++
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
+		fmt.Printf("%s%d", key, counter[key])
+	}
+}
